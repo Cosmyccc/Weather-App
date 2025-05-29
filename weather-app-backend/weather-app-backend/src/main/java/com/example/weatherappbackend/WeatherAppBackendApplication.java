c8a2e93351f8912fcce0d78a5915a00589204c2d
Rani <103280525+Rani1303@users.noreply.github.com>
2025-05-29 15:01:18 +0530

    Fix: Changes in Docker Done

--- weather-app-backend/weather-app-backend/src/main/java/com/example/weatherappbackend/WeatherAppBackendApplication.java
+++ weather-app-backend/weather-app-backend/src/main/java/com/example/weatherappbackend/WeatherAppBackendApplication.java
@@ -2,12 +2,28 @@ package com.example.weatherappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WeatherAppBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost") // Allow requests from your frontend origin
+						.allowedMethods("*") // Allow all methods (GET, POST, PUT, DELETE, etc.)
+						.allowedHeaders("*"); // Allow all headers
+			}
+		};
 	}
 
 }
