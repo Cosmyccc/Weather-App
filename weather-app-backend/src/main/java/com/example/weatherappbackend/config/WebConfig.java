ab28efe1892599c032ca5d230ea3466da19678d9
Rani <103280525+Rani1303@users.noreply.github.com>
2025-05-29 18:00:18 +0530

    Fix

--- weather-app-backend/src/main/java/com/example/weatherappbackend/config/WebConfig.java
+++ weather-app-backend/src/main/java/com/example/weatherappbackend/config/WebConfig.java
@@ -9,10 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", 
-                "https://weather-app-ten-mu-56.vercel.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins(
+                    "http://localhost:5173",
+                    "https://weather-app-ten-mu-56.vercel.app"
+                )
+                .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 } 
